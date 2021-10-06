0b45752976cb0680cfc5e032e19888c01126b970
SoPlays <joao.ranger@gmail.com>
2021-10-06 23:35:45 +0100

    first commit

---
+++ VanillaJSProjects/#1-ColorFlipper/hex.js
@@ -0,0 +1,22 @@
+const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+// #f15025
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+
+    let hexColor = '#';
+    for(let i = 0 ; i<=6 ; i++){
+        hexColor += hex[getRandomHexChar()];
+    }
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
+
+function getRandomHexChar(){
+    hexcolor = Math.floor(Math.random()*hex.length)
+    
+    console.log(hexColor)
+    return hexColor;
+}
---
+++ VanillaJSProjects/#2-Counter/app.js
@@ -0,0 +1,13 @@
+
+//set initial count
+let count = 0
+
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+console.log(btns)
+
+btns.forEach(function (btn){
+
+})
